133111b93e00e83347f5499be612a4e85277bc7a
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2026-05-05 00:39:32 +0900

    pnpm testはDBのテストを行わないように変更

--- apps/kaede/vitest.config.ts
+++ apps/kaede/vitest.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    globalSetup: ['./tests/db/global-setup.ts'],
-    setupFiles: ['./tests/db/setup.ts'],
-    include: ['src/**/*.test.ts', 'tests/**/*.test.ts'],
+    include: ['src/**/*.test.ts'],
   },
 })
